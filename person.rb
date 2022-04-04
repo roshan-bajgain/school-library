5ff94ca5af3dba0e0ab78c98938358d49ef57503
Roshan Bajgain <roshan.bazgain@gmail.com>
2022-04-04 20:14:38 +0545

    use constructor for age,name,etc

--- person.rb
+++ person.rb
@@ -2,4 +2,10 @@ class Person
     attr_reader :id
     attr_accessor :name, :age
 
+    def initialize(age, name = 'unknown', parent_permission: true)
+        @id = Randon.rand(1..1000)
+        @name = name
+        @age = age
+        @parent_permission = parent_permission
+    end    
 end    
