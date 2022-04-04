8d991f30273c8ba13c1aa2086abaa1672305a546
Roshan Bajgain <roshan.bazgain@gmail.com>
2022-04-04 20:21:21 +0545

    use private and public method to check statement

--- person.rb
+++ person.rb
@@ -8,4 +8,16 @@ class Person
         @age = age
         @parent_permission = parent_permission
     end    
+
+    private
+
+    def of_age?
+        @age >=18
+    end
+    
+    public
+
+    def can_use_services?
+        of_age? || parent_permission
+    end    
 end    
