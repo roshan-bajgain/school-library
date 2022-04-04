4db9242aaa359f9e5d00d344c9128899978827eb
Roshan Bajgain <roshan.bazgain@gmail.com>
2022-04-04 20:27:48 +0545

    Created constructor for student class

---
+++ student.rb
@@ -0,0 +1,9 @@
+require './person'
+
+class Student < person
+    attr_accessor :classroom
+    
+    def initialize(classroom,age,name,parent_permission)
+        super(age,name,parent_permission)
+        @classroom = classroom
+    end    
