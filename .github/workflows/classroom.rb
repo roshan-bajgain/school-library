3815b3618f3abbd6fc408afb597b55750d881f2d
Roshan Bajgain <roshan.bazgain@gmail.com>
2022-04-07 13:38:00 +0545

    create relation between student and classroom

--- .github/workflows/classroom.rb
+++ .github/workflows/classroom.rb
@@ -1,6 +1,11 @@
 class Classroom 
-    attr_accessor :label
+    attr_accessor :label :students
     def initialize
         @label = label
+        @students = []
     end
+    def add_student(student)
+        @student.push(student)
+        student.classroom = self
+    end    
 end        
