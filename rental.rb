7fed8bf620e97d7591c00f0f8b4ee1e3133d6fd2
Roshan Bajgain <roshan.bazgain@gmail.com>
2022-04-07 14:12:03 +0545

    Add relatio between person and rental

--- rental.rb
+++ rental.rb
@@ -1,7 +1,11 @@
 class Rental
-    attr_accessor :data
+    attr_accessor :data :book :person
 
-    def initialize(data)
+    def initialize(data, book, person)
         @data = data
+        @book = book
+        @person = person
+        person.rental << self
+        book.rental << self
     end
 end
