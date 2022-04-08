cb53581eee56f138933ef94596af16f34d0a9989
Roshan Bajgain <roshan.bazgain@gmail.com>
2022-04-08 14:33:32 +0545

    Add class app and create method

---
+++ app.rb
@@ -0,0 +1,14 @@
+require './person'
+require './student'
+require './teacher'
+require './book'
+require './classroom'
+require './rental'
+
+class App
+    def initialize
+      @books = []
+      @persons = []
+      @rentals = []
+    end
+end    
