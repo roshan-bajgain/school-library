11288ff021a9bea1c94a3dd1362547a8f41dee5e
jihaneh <jijih@hotmail.fr>
2022-04-11 17:38:58 +0200

    Refactor create-rental

--- app.rb
+++ app.rb
@@ -6,6 +6,7 @@ require './classroom'
 require './rental'
 require './lib/create_books'
 require './lib/create_persons'
+require './lib/create_rental'
 
 class App
   def initialize
@@ -58,25 +59,7 @@ class App
   end
 
   def create_rental
-    puts 'Select which book you want to rent by entering its number'
-    @books.each_with_index { |book, index| puts "#{index}) Title: #{book.title}, Author: #{book.author}" }
-
-    book_id = gets.chomp.to_i
-
-    puts 'Select a person from the list by its number'
-    @persons.each_with_index do |person, index|
-      puts "#{index}) [#{person.class.name}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
-    end
-
-    person_id = gets.chomp.to_i
-
-    print 'Date: '
-    date = gets.chomp.to_s
-
-    rental = Rental.new(date, @persons[person_id], @books[book_id])
-    @rentals << rental
-
-    puts 'Rental created successfully'
+    @rentals << CreateRental.new.create_rental(@books, @persons)
   end
 
   def list_all_rentals
