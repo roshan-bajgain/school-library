1b549874e57f7c821d63301bc77a60fc7105a610
Roshan Bajgain <roshan.bazgain@gmail.com>
2022-04-13 19:29:08 +0545

    Add test for the student, book, person

---
+++ spec/book_spec.rb
@@ -0,0 +1,23 @@
+require_relative '../book'
+require_relative '../rental'
+
+describe "Book" do
+    before(:each) do
+        @book = Book.new("Harry poter", "jk rouling")
+    end
+    it "Take title and author" do
+        @book.author.should eql "jk rouling"
+        @book.title.should eql "Harry poter"
+    end
+    # it "Add new book renter" do
+    #     @rental = rental.new("2022-01-02", "Roshan")
+    #     @book.add_rental(@rental)
+    #     @expect(@book.rentals).should eq([@rental])
+    # end
+    # it "Add new book rental" do
+    #     @book.add_rental(person.new( "John Doe", "2000-08-20"))
+    #     @book.rentals.should eql [person.new("John Doe", "2000-08-20")]
+    #   end
+end    
+
+
---
+++ spec/student_spec.rb
@@ -0,0 +1,13 @@
+require_relative '../student'
+
+describe "Studen" do
+    before(:each) do
+        @student = Student.new("22","Harry poter", "true")
+    end
+    it "Take age, name and parent_permission" do
+        @student.age.should eql "22"
+        @student.name.should eql "Harry poter"
+        @student.parent_permission.should eql "true"
+
+    end
+end    
