27688cd0250593bf8811f1de9c63b7dcd27b2b79
Roshan Bajgain <roshan.bazgain@gmail.com>
2022-04-13 19:30:17 +0545

    Fix linter error

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -1,12 +1,12 @@
 require_relative '../person'
 
-describe "Person" do
-    before(:each) do
-        @person = Person.new("55", "roshan",  parent_permission: true)
-    end
-    it "Take age, name and parent_permission" do
-        @person.age.should eql "55"
-        @person.name.should eql "roshan"
-        @person.parent_permission.should eql true
-    end
-end    
+describe 'Person' do
+  before(:each) do
+    @person = Person.new('55', 'roshan', parent_permission: true)
+  end
+  it 'Take age, name and parent_permission' do
+    @person.age.should eql '55'
+    @person.name.should eql 'roshan'
+    @person.parent_permission.should eql true
+  end
+end
--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -1,13 +1,12 @@
 require_relative '../student'
 
-describe "Studen" do
-    before(:each) do
-        @student = Student.new("22","Harry poter", "true")
-    end
-    it "Take age, name and parent_permission" do
-        @student.age.should eql "22"
-        @student.name.should eql "Harry poter"
-        @student.parent_permission.should eql "true"
-
-    end
-end    
+describe 'Studen' do
+  before(:each) do
+    @student = Student.new('22', 'Harry poter', 'true')
+  end
+  it 'Take age, name and parent_permission' do
+    @student.age.should eql '22'
+    @student.name.should eql 'Harry poter'
+    @student.parent_permission.should eql 'true'
+  end
+end
